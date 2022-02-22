7be3318fe75d3f43cad226031e2bef0226e6c038
Aritra <aet419@gmail.com>
2022-02-22 12:32:11 +0000

    added node-pty (operational)

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const { Server } = require("socket.io");
+const os = require('os');
+const pty = require('node-pty');
 
-const io = new Server(3444,{
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+// init socket.io connection
+const io = new Server(3444, {
     cors: '*'
 });
 
+// listen for connection
 io.on("connection", (socket) => {
-    console.log('New connection:',socket.id);
+    console.log('New connection:', socket.id);
+
+    // init shell process
+    const ptyProcess = pty.spawn(shell, [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 30,
+        cwd: process.env.HOME,
+        env: process.env
+    });
+    ptyProcess.on('data', function (data) {
+        // process.stdout.write(data);
+        socket.emit('resp', data)
+    });
+
+    socket.on('keystroke', data => {
+        ptyProcess.write(data);
+    });
+   socket.on('disconnect',reason => {
+       console.log(reason, socket.id);
+        ptyProcess.kill();
+   });
 });
