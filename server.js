89b3593e4fc3a35a637c5079c853e5a1613e83e8
Aritra <aet419@gmail.com>
2022-02-22 10:37:21 +0000

    added nodemon and cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { Server } = require("socket.io");
 
-const io = new Server(3444);
+const io = new Server(3444,{
+    cors: '*'
+});
 
 io.on("connection", (socket) => {
     console.log('New connection:',socket.id);
