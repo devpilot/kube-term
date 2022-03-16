a52fb34a3c603b871c08d7a2e2a7b1fa0b85065d
Aritra <aet419@gmail.com>
2022-03-16 19:28:12 +0000

    replace socket.io with ws

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const { createServer } = require("http");
-const { Server } = require("socket.io");
+const { WebSocketServer } = require("ws");
 const os = require('os');
 const pty = require('node-pty');
 
 const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
 const app = express();
-const httpServer = createServer(app);
+const server = createServer(app);
 
-// init socket.io connection
-const io = new Server(httpServer, {
-    cors: '*'
-});
+// init socket connection
+const wss = new WebSocketServer({ server })
 
 // serve index file
 app.get('/', function (req, res) {
@@ -21,22 +19,18 @@ app.get('/', function (req, res) {
 
 // Expose the node_modules folder as static resources
 app.use('/xterm', express.static('node_modules/xterm'));
-app.use('/socket.io', express.static('node_modules/socket.io/client-dist'));
+// app.use('/socket.io', express.static('node_modules/socket.io/client-dist'));
 
 // The server should start listening
-httpServer.listen(3443, '0.0.0.0', () => console.log('Server started on http:// 0.0.0.0:3443'));
+server.listen(3443, '0.0.0.0', () => console.log('Server started on http:// 0.0.0.0:3443'));
 
 // listen for connection
-io.on("connection", (socket) => {
-    console.log('New connection:', socket.id);
+wss.on("connection", (ws, req) => {
+    console.log('New connection: ', req.socket.remoteAddress);
 
     // init shell process
     const ptyProcess = pty.spawn(shell,
-        [
-            "./pty.sh",
-            socket.handshake.query.user || '',
-            socket.handshake.query.host || ''
-        ], {
+        ["./pty.sh"], {
         name: 'xterm-256color',
         cols: 100,
         rows: 30,
@@ -46,15 +40,15 @@ io.on("connection", (socket) => {
 
     ptyProcess.onData(data => {
         // process.stdout.write(data);
-        socket.emit('resp', data)
+        ws.send(data)
     });
 
-    socket.on('keystroke', data => {
+    ws.on('message', data => {
         ptyProcess.write(data);
     });
 
-    socket.on('disconnect', reason => {
-        console.log(reason, socket.id);
+    ws.on('disconnect', reason => {
+        console.log(reason);
         ptyProcess.kill();
     });
 
